db8c4157976a12c496336e895255d3abc19c07fa
subratnandi <90413227+subratnandi@users.noreply.github.com>
2021-09-12 12:29:29 +0530

    Add files via upload

---
+++ p7if else.c
@@ -0,0 +1,20 @@
+ #include<stdio.h>
+ main()
+ {
+ 	int age;
+ 	printf("Enter the age = ");
+ 	scanf("%d",&age);
+ 	if(age<18)
+ 	{
+ 		printf("you can not vote");
+ 	 }
+ 	 else if(age>=18 && age<26)
+ 	 {
+ 	 	printf("you are minar voter and give vote");
+	  }
+	  else{
+	  
+	  printf("you can gave vote");}
+return 0;
+ }
+ 
